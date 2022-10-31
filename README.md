e794af47149abe635e41e2f855abc1db863885e5
Jade <y3m45566@gmail.com>
2022-10-31 15:55:21 +0800

    set up vue3 environment.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# NCKU-SDSN-website
+# 成功大學 SDSN website
+
+## 協作人員
+
+Rex, Vic, 季芳, 子玉
+
+## 技術使用
+
+- Vue3 + Vite
+- bootstrap
+- swiper
+- sass
+
+## 時間
+
+11/29 驗收
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <nav>1111</nav>
+  </header>
+</template>
+
+<style scoped></style>
